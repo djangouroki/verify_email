795b43fe8680b2848994cc0abbfdaae1ee477962
djangouroki <django.uroki@mail.ru>
2022-01-05 05:58:59 +0300

    email authenticate

---
+++ project/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('users/', include('users.urls')),
+]
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from users.views import Register
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+
+    path('register/', Register.as_view(), name='register'),
+]
